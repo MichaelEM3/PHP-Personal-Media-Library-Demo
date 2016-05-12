cb9f35c6962ce91dd84e5c710f113c871fc2e53d
Michael <michaelem3@gmail.com>
2016-05-12 19:14:53 -0400

    Created a footer file

--- index.php
+++ index.php
@@ -20,22 +20,4 @@ the same PHP, HTML, or text on multiple pages of a website. -->
 
 		</div>
 
-	</div><!-- end content -->
-
-	<div class="footer">
-
-		<div class="wrapper">
-
-			<ul>
-				<li><a href="http://twitter.com/treehouse">Twitter</a></li>
-				<li><a href="https://www.facebook.com/TeamTreehouse">Facebook</a></li>
-			</ul>
-
-			<p>&copy;<?php echo date("Y"); ?> Personal Media Library</p>
-
-		</div>
-
-	</div>
-
-</body>
-</html>
+<?php include("includes/footer.php"); ?>
