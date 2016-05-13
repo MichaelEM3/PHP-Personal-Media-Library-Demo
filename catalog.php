f7d8cc8cefe9b27fbecead7b59c96ae26891b3e8
Michael <michaelem3@gmail.com>
2016-05-12 20:12:42 -0400

    Fixed catalog routes using _GET variable function

--- catalog.php
+++ catalog.php
@@ -2,10 +2,20 @@
 
 $pageTitle = "Full Catalog";
 
+if (isset($_GET["cat"])) {
+  if ($_GET["cat"] == "books") {
+    $pageTitle = "Books";
+  } else if ($_GET["cat"] == "movies") {
+    $pageTitle = "Movies";
+  } else if ($_GET["cat"] == "music") {
+    $pageTitle = "Music";
+  }
+}
+
 include("includes/header.php"); ?>
 
 <div class="section page">
-  <h1>Full Catalog</h1>
+  <h1><?php echo $pageTitle; ?></h1>
 </div>
 
 <?php include("includes/footer.php"); ?>
