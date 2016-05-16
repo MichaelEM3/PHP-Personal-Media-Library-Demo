97905a429d04255ee373ed7267ec2ab89f9792b4
Michael <michaelem3@gmail.com>
2016-05-16 18:55:15 -0400

    Improved array formatting, manually assigned array keys

--- catalog.php
+++ catalog.php
@@ -1,10 +1,24 @@
 <?php
 
-$catalog = array(
-  "Design Patterns",
-  "Forrest Gump",
-  "Beethoven"
-);
+//it's good practice to start with an empty array.
+//101 is us assigning a key to the value of "Design Patterns", PHP lets you set keys yourself
+//we're giving books keys in the 100s, movies keys in the 200s, music the 300s
+
+$catalog = array();
+$catalog[101] = "Design Patterns";
+$catalog[201] = "Forrest Gump";
+$catalog[301] = "Beethoven";
+
+//Original way we created this array. See above for best practice.
+// $catalog = array(
+//   "Design Patterns",
+//   "Forrest Gump",
+//   "Beethoven"
+// );
+
+$catalog[102] = "Clean Code";
+//tells PHP to add one new element to the array using whatever index is available next.
+//PHP will assign an index of 3. Design Patterns = 0, remember.
 
 $pageTitle = "Full Catalog";
 $section = null;
@@ -35,6 +49,8 @@ include("includes/header.php"); ?>
     <h1><?php echo $pageTitle; ?></h1>
 
     <ul>
+      <!--using a foreeach loop to print all the catalog array items to the page -->
+      <!--$item is a variable name we created just for this loop -->
       <?php foreach($catalog as $item) {
         echo "<li>" . $item . "</li>";
       }
